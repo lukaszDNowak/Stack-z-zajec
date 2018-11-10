b4668f324eaa18a9fff9981f417159b0e6c2a77f
Lukasz Nowak <lukasz.nowak1@microsoft.wsei.edu.pl>
2018-11-10 17:02:02 +0100

    Add project files.

---
+++ Stackk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stackk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTestMyStack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using stack;
+using NUnit.Framework;
+
+namespace StackTest
+{
+
+    public class StackUnitTest
+    {
+        [Test]
+        public void TestPush()
+        {
+            Stack<int> st = new Stack<int>();
+            st.Push(200);
+
+            Assert.AreEqual(st.Pop(), 200);
+        }
+        [Test]
+        public void TestPop()
+        {
+            Stack<string> st = new Stack<string>();
+            st.Push("S");
+            st.Push("T");
+            st.Push("A");
+            
+            Assert.AreEqual(st.Pop(), "A");
+            Assert.AreEqual(st.Pop(), "T");
+            Assert.AreEqual(st.Pop(), "S");
+        }
+
+        [Test]
+        public void TestPopExeption()
+        {
+            Stack<object> st = new Stack<object>();
+            Assert.That(() => st.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void TestPeekExeption()
+        {
+            Stack<object> st = new Stack<object>();
+            Assert.That(() => st.Peek(), Throws.InvalidOperationException);
+        }
+        [Test]
+        public void TestPushExeption()
+        {
+            Stack<object> st = new Stack<object>();
+            Assert.That(() => st.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void TestPeek()
+        {
+            Stack<string> st = new Stack<string>();
+            st.Push("S");
+            st.Push("T");
+            st.Push("A");
+
+            Assert.AreSame(st.Peek(), "A");
+        }
+
+        [Test]
+        public void StackIsEmpty()
+        {
+            Stack<string> st = new Stack<string>();
+
+            Assert.AreEqual(st.Count, 0);
+        }
+        [Test]
+        public void StackIsNotEmpty()
+        {
+            Stack<object> st = new Stack<object>();
+            st.Push("Nie jest pusty");
+            Assert.AreEqual(st.Count, 1);
+        }
+    }
+}
